ccd390d5a41a130d59946ca64d98bfca1a8deae5
Matt <matt@encrypt-s.com>
2017-05-23 11:59:22 +1200

    Swapped css for scss

--- src/app/selling-points-sect/selling-points-sect.component.ts
+++ src/app/selling-points-sect/selling-points-sect.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'selling-points-sect',
   templateUrl: './selling-points-sect.component.html',
-  styleUrls: ['./selling-points-sect.component.css']
+  styleUrls: ['./selling-points-sect.component.scss']
 })
 export class SellingPointsSectComponent implements OnInit {
 
