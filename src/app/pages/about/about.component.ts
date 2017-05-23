6eadd0a2aa990848f9350ea0d7bf432e817ef247
Matt <matt@encrypt-s.com>
2017-05-24 10:51:56 +1200

    updated about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD:src/app/about-page/about-page.component.ts
-  selector: 'app-about-page',
-  templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.scss']
-=======
   selector: 'about-page',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
->>>>>>> master:src/app/pages/about/about.component.ts
+  styleUrls: ['./about.component.scss']
 })
 export class AboutPage implements OnInit {
 
