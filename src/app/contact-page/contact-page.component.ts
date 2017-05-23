c59297cc0c0307d268e008ae202465e01db888b4
Matt <matt@encrypt-s.com>
2017-05-24 10:17:43 +1200

    Updated contact page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
 
