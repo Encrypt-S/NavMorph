152178880f7d7b3b11ec665b1c522cbe014b6030
Matt <matt@encrypt-s.com>
2017-05-23 15:03:01 +1200

    Swapped to scss

--- src/app/tile-component/tile-component.component.ts
+++ src/app/tile-component/tile-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tile-component',
   templateUrl: './tile-component.component.html',
-  styleUrls: ['./tile-component.component.css']
+  styleUrls: ['./tile-component.component.scss']
 })
 export class TileComponentComponent implements OnInit {
 
