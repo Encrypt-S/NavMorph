d315bbff82ebbfa1afcbeceb418e20d1c7c629d4
Matt <matt@encrypt-s.com>
2017-05-24 10:53:15 +1200

    updated send page

--- src/app/pages/send/send.component.ts
+++ src/app/pages/send/send.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD:src/app/image-with-text/image-with-text.component.ts
-  selector: 'app-image-with-text',
-  templateUrl: './image-with-text.component.html',
-  styleUrls: ['./image-with-text.component.scss']
-=======
   selector: 'send-page',
   templateUrl: './send.component.html',
-  styleUrls: ['./send.component.css']
->>>>>>> master:src/app/pages/send/send.component.ts
+  styleUrls: ['./send.component.scss']
 })
 export class SendPage implements OnInit {
 
