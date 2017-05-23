b4cb5f8e063c1184fde0724c4f55957fb66fff18
Matt <matt@encrypt-s.com>
2017-05-24 10:52:18 +1200

    updated contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD:src/app/contact-page/contact-page.component.ts
-  selector: 'app-contact-page',
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
-=======
   selector: 'contact-page',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
->>>>>>> master:src/app/pages/contact/contact.component.ts
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactPage implements OnInit {
 
