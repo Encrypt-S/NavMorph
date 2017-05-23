08f523dbf3767d80a4967e700133411a394c7e78
Matt <matt@encrypt-s.com>
2017-05-24 10:23:19 +1200

    updated image with text

--- src/app/image-with-text/image-with-text.component.ts
+++ src/app/image-with-text/image-with-text.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-image-with-text',
   templateUrl: './image-with-text.component.html',
-  styleUrls: ['./image-with-text.component.css']
+  styleUrls: ['./image-with-text.component.scss']
 })
 export class ImageWithTextComponent implements OnInit {
 
