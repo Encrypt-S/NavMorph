0fef590f299a5b54f14bbf616cc44d9d032afdd4
Matt <matt@encrypt-s.com>
2017-05-23 15:01:51 +1200

    Initialized component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SendPageComponent } from './send-page/send-page.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { TileComponentComponent } from './tile-component/tile-component.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { HowItWorksComponent } from './how-it-works/how-it-works.component';
     HomePageComponent,
     SendPageComponent,
     HowItWorksComponent,
+    TileComponentComponent,
   ],
   imports: [
     FormsModule, 
---
+++ src/app/tile-component/tile-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tile-component',
+  templateUrl: './tile-component.component.html',
+  styleUrls: ['./tile-component.component.css']
+})
+export class TileComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
