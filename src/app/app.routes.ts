81f48df627bf637f495530988ea9c6011cb575c7
Matt <matt@encrypt-s.com>
2017-05-25 09:57:07 +1200

    created new routes file, moved routes into it, imported file into app.module

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { DemoPage } from './pages/demo/demo.component';
+import { AboutPage } from './pages/about/about.component';
+import { ContactPage } from './pages/contact/contact.component';
+import { HomePage } from './pages/home/home.component';
+import { SendPage } from './pages/send/send.component';
+
+export const routes: Routes = [
+  { path: 'demo', component: DemoPage},
+  { path: '', component: HomePage},
+  { path: 'send', component: SendPage},
+  { path: 'about', component: AboutPage},
+  { path: 'contact', component: ContactPage},
+];
