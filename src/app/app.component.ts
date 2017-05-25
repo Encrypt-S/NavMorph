31a920c8eee419a74e51c27aca48bf85484313d7
Matt <matt@encrypt-s.com>
2017-05-26 11:20:39 +1200

    removed service, dont need it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 
 import { TitleChangeService } from './services/title-change/title-change';
-import { tabIndicatorUpdateService } from './services/tab-indicator-update/tab-indicator-update';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [TitleChangeService, tabIndicatorUpdateService]
+  providers: [TitleChangeService]
 })
 export class AppComponent {
 
@@ -18,7 +17,6 @@ export class AppComponent {
       _router.events.subscribe ( event => {
         if( event instanceof NavigationStart ){
           titleChangeService.updateTitle(event);
-          tabIndicatorUpdateService.updateTab(event);
         }
       });
     }
