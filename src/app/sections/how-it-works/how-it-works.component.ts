5068258e46ac7478b856857f200bfd90e928192e
Matt <matt@encrypt-s.com>
2017-05-29 14:52:43 +1200

    added about section to about-page. Added ngIf to the about-section button to show/hide it.

--- src/app/sections/how-it-works/how-it-works.component.ts
+++ src/app/sections/how-it-works/how-it-works.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'how-it-works-section',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowItWorksSection implements OnInit {
 
+  @Input('showButton')
+  showButton: boolean = true;
   constructor() { }
 
   ngOnInit() {
