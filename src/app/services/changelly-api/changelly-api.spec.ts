22169c8e5d65c39a533296393aa4b70709ad3bf6
Matt <matt@encrypt-s.com>
2017-06-06 16:11:52 +1200

    more unit test fixes

--- src/app/services/changelly-api/changelly-api.spec.ts
+++ src/app/services/changelly-api/changelly-api.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { ChangellyApiService } from './changelly-api';
+
+import { GenericNodeApiService } from './../../services/generic-node-api/generic-node-api';
+
 
 describe('ChangellyApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ChangellyApiService]
+      imports: [
+        HttpModule,
+      ],
+      providers: [
+        ChangellyApiService,
+        GenericNodeApiService,
+      ]
     });
   });
 
