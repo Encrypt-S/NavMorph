8d5700e9992a7fcfc25798dbd49fbccc41edc7df
Matt <matt@encrypt-s.com>
2017-06-13 16:35:23 +1200

    Added error validation to the send-page-data service. Some validation is yet to be implemented as its needs functionality that is out of scope. pulled some consts into the services config file.

--- src/app/services/send-page-data/send-page-data.ts
+++ src/app/services/send-page-data/send-page-data.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { ChangellyApiService } from '../../services/changelly-api/changelly-api';
+import { blankDataBundle, changellyConstData } from "../config";
 
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs/Subject';
@@ -9,22 +10,6 @@ import 'rxjs/add/operator/first';
 @Injectable()
 export class SendPageDataService {
 
-  transferAmount: number
-  originCoin: string
-  destCoin: string
-  destAddr: string
-
-  estTime: object
-  estConvToNav
-  estConvFromNav
-
-  changellyFeeTotalToNav: number
-  changellyFeeTotalFromNav: number
-  navtechFeeTotal: number
-  changellyFee: number = 0.995
-  navtechFee: number = 0.995
-  MAX_NAV_PER_TRADE: number = 10000
-
   dataBundle = {
     'transferAmount': undefined,
     'originCoin': undefined,
@@ -32,17 +17,25 @@ export class SendPageDataService {
     'destAddr': undefined,
     'estConvToNav': undefined,
     'estConvFromNav': undefined,
+    'estTime': undefined,
     'changellyFeeTotalToNav': undefined,
     'navtechFeeTotal': undefined,
     'changellyFeeTotalFromNav': undefined,
+    'minTransferAmount': undefined,
+    'validData': false,
     'errors': {
       'invalidDestAddress': false,
+      'invalidTransferAmount': false,
       'transferTooSmall': false,
       'transferTooLarge': false,
       'navToNavTransfer': false,
       'changellyError': false
     }
   }
+
+  CHANGELLY_FEE: number = changellyConstData.CHANGELLY_FEE
+  NAVTECH_FEE: number = changellyConstData.NAVTECH_FEE
+  MAX_NAV_PER_TRADE: number = changellyConstData.MAX_NAV_PER_TRADE
 
   subject = new Subject<any>()
 
@@ -59,53 +52,82 @@ export class SendPageDataService {
   }
 
   clearData(): void {
-    this.transferAmount = undefined
-    this.originCoin = undefined
-    this.destCoin = undefined
-    this.destAddr = undefined
+    this.dataBundle = blankDataBundle
     this.isDataSet = false
+    this.subject.next(this.dataBundle)
+  }
+
+  checkIsDataSet():boolean {
+    return this.isDataSet
   }
 
   storeData(transferAmount, originCoin, destCoin, destAddr): void {
-    this.transferAmount = transferAmount
-    this.originCoin = originCoin
-    this.destCoin = destCoin
-    this.destAddr = destAddr
-
     this.dataBundle.transferAmount = transferAmount
     this.dataBundle.originCoin = originCoin
     this.dataBundle.destCoin = destCoin
     this.dataBundle.destAddr = destAddr
+    this.validateFormData(this.dataBundle)
 
-    this.getEstimatedExchange(originCoin, 'nav', this.transferAmount)
+    this.getEstimatedExchange(originCoin, 'nav', transferAmount)
       .then((data) => {
-        this.dataBundle.changellyFeeTotalToNav = transferAmount - transferAmount * this.changellyFee
-        this.dataBundle.navtechFeeTotal = data - data * this.navtechFee
+        this.dataBundle.changellyFeeTotalToNav = transferAmount - transferAmount * this.CHANGELLY_FEE
+        this.dataBundle.navtechFeeTotal = data - data * this.NAVTECH_FEE
         this.dataBundle.estConvToNav = data - this.dataBundle.navtechFeeTotal
-        this.getEstimatedExchange('nav', destCoin, this.transferAmount)
+
+        this.getEstimatedExchange('nav', destCoin, transferAmount)
         .then((data) => {
-          this.dataBundle.changellyFeeTotalFromNav = data * this.changellyFee
+          this.dataBundle.changellyFeeTotalFromNav = data * this.CHANGELLY_FEE
           this.dataBundle.estConvFromNav = data - this.dataBundle.changellyFeeTotalFromNav
-          this.subject.next(this.dataBundle)
           this.isDataSet = true
+
+          this.validateDataBundle(this.dataBundle)
+          .then(() => this.subject.next(this.dataBundle))
         })
     })
+
   }
 
-  validateFormData(dataBundle) {
+  validateFormData(dataBundle):boolean {
+    let isValid = true
+    if(!(dataBundle.transferAmount instanceof Number)){
+      dataBundle.errors.invalidTransferAmount = true
+      isValid = false
+      dataBundle.validData = false
+    }
     if(dataBundle.originCoin === 'nav' && dataBundle.destCoin === 'nav') {
       dataBundle.errors.navToNavTransfer = true
+      isValid = false
+      dataBundle.validData = false
     }
     if(dataBundle.transferAmount < this.getMinTransferAmount(dataBundle.originCoin, 'nav')) {
       dataBundle.errors.transferTooSmall = true
+      isValid = false
+      dataBundle.validData = false
     }
-    if(dataBundle.transferAmount > this.MAX_NAV_PER_TRADE) {
-      dataBundle.errors.transferTooLarge = true
-    }
+    return isValid
+  }
+
+  validateDataBundle(dataBundle) {
+    return new Promise<any>( resolve => {
+      if(dataBundle.estConvToNav > this.MAX_NAV_PER_TRADE) {
+        dataBundle.errors.transferTooLarge = true
+        dataBundle.validData = false
+      }
+      // if( !checkAddress(dataBundle.destAddr)) {
+        // dataBundle.errors.invalidDestAddress  = true
+      // }
+      // if(changellyError () {
+        // dataBundle.errors.changellyError = true
+      // }
+      resolve()
+    })
   }
 
   getEstimatedExchange(originCoin, destCoin, transferAmount) {
     return new Promise<any>( resolve => {
+      if(originCoin === 'nav' && destCoin === 'nav'){
+        resolve(transferAmount)
+      }
       this.changellyApi.getExchangeAmount(originCoin, destCoin, transferAmount)
       .subscribe( data => {
         resolve(data)
