0a68410a1ee1ca66bbdaa631935df8369e0e4abd
Matt <matt@encrypt-s.com>
2017-06-26 12:27:32 +1200

    added new rpc to generate navcoin addresses for receiving nav

---
+++ server/lib/rpc/get-new-address.js
@@ -0,0 +1,31 @@
+const Rpc = require('./client')
+
+Rpc.getNewAddress = (req, res) => {
+  try {
+    Rpc.navClient.getNewAddress().then((data) => {
+      res.send(JSON.stringify({
+        status: 200,
+        type: 'SUCCESS',
+        data,
+      }))
+    }).catch((e) => {
+      res.send(JSON.stringify({
+        status: 200,
+        type: 'FAIL',
+        code: 'RPC_002',
+        message: 'something went wrong',
+        error: e,
+      }))
+    })
+  } catch (e) {
+    res.send(JSON.stringify({
+      status: 200,
+      type: 'FAIL',
+      code: 'RPC_001',
+      message: 'something went wrong',
+      error: e,
+    }))
+  }
+}
+
+module.exports = Rpc
