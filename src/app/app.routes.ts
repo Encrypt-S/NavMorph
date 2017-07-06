435017b8dd57b584e9d9b199364dba0f6811f136
Matt <matt@encrypt-s.com>
2017-07-06 17:54:53 +1200

    added the beginnings of the status page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,6 @@ export const routes: Routes = [
   { path: '', component: HomePage},
   { path: 'send', component: SendPage},
   { path: 'about', component: AboutPage},
+  { path: 'status/:id/:password', component: StatusPage},
   { path: '**', redirectTo: ''},
 ];
---
+++ src/app/pages/status/status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'status-page',
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.scss']
+})
+export class SendPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
