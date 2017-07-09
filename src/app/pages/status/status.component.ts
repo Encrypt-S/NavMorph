fcff7483bca9c3a4b81a9f18a52d37fa5130e7a6
Matt <matt@encrypt-s.com>
2017-07-10 10:58:08 +1200

    added methods to status component

--- src/app/pages/status/status.component.ts
+++ src/app/pages/status/status.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from '../../services/order/order'
+import { QRCodeModule } from 'angular2-qrcode';
+
 
 @Component({
   selector: 'status-page',
   templateUrl: './status.component.html',
-  styleUrls: ['./status.component.scss']
-})
-export class SendPage implements OnInit {
+  styleUrls: ['./status.component.scss'],
 
-  constructor() { }
+  providers: [
+    OrderService,
+  ],
+})
+export class StatusPage implements OnInit {
+
+  isLoading: boolean = true
+  orderId: string
+  orderPass: string
+  orderData: object
+
+  constructor(
+    private OrderService: OrderService,
+    private router: Router,
+   ) {}
 
   ngOnInit() {
+    this.parseUrl(this.router.url)
+    this.getOrderData()
   }
 
+  parseUrl (url: string){
+    const split = url.split('/')
+    this.orderId = split[2]
+    this.orderPass = split[3]
+  }
+
+  getOrderData() {
+    this.OrderService.getOrder(this.orderId, this.orderPass)
+    .subscribe(data => {
+      console.log(data)
+      console.log(data[0])
+      this.orderData = data[0]
+      this.isLoading = false
+    })
+  }
 }
