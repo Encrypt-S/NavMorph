8479be884102ab8fbd3c0f28f228e10cc6439113
Matt <matt@encrypt-s.com>
2017-07-20 17:27:40 +1200

    added failedLogins model

---
+++ server/lib/db/failedLogins.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+const mongoDB = 'mongodb://127.0.0.1/polymorph'
+mongoose.connect(mongoDB)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// Define schema
+
+const Schema = mongoose.Schema
+
+const failedLogin = new Schema({
+  ip_address: { type: String, required: true },
+  polymorph_id: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+  params: { type: String, required: true },
+})
+
+// Compile model from schema
+let failedLoginModel = mongoose.model('failedLogin', failedLogin) //eslint-disable-line
+
+module.exports = failedLoginModel
