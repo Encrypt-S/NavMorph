a9561c4e36729406193e831dc583ce25985efebf
Matt <matt@encrypt-s.com>
2017-07-20 17:27:29 +1200

    added blacklist model

---
+++ server/lib/db/blackList.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+const mongoDB = 'mongodb://127.0.0.1/polymorph'
+mongoose.connect(mongoDB)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// Define schema
+
+const Schema = mongoose.Schema
+
+const BlackList = new Schema({
+  ip_address: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+})
+
+// Compile model from schema
+let BlackListModel = mongoose.model('BlackList', BlackList) //eslint-disable-line
+
+module.exports = BlackListModel
