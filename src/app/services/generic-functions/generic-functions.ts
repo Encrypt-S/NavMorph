fd21dba14e8061d417e5a6896a330c14bcc60fe9
Matt <matt@encrypt-s.com>
2017-09-07 16:41:44 +1200

    removed random line of testing code

--- src/app/services/generic-functions/generic-functions.ts
+++ src/app/services/generic-functions/generic-functions.ts
@@ -8,8 +8,6 @@ export class GenericFunctionsService {
   constructor() { }
 
   calculateOrderEst(minutes) {
-    jasmine.clock().install();
-
     let estimate = new Date()
     estimate.setMinutes(estimate.getMinutes() + minutes)
     return estimate
