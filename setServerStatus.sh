a06c1f6741dd0a54857e42c71dff1fbc14c3c34e
Matt <matt@encrypt-s.com>
2017-09-15 11:20:56 +1200

    added a bash script for updating the server status

---
+++ setServerStatus.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# sysinfo_page - A script to update the server mode
+
+if [ $# lt 2 ]; then
+    echo "You're missing a server status argument."
+    echo "Usage: setServerStatus <status>"
+else if [ $# gt 2 ]; then
+    echo "You have used too many arguments."
+    echo "Usage: setServerStatus <status>"
+else
+  set_status()
+fi
+
+
+set_status () { 
+  echo "Updating server status to $1"
+  
+  mongo polymorph --eval "db.server_mode.update({}, {'server_mode': $1})"
+  mongo polymorph --eval "db.server_mode.update({}, {'server_mode': $1})"
+  
+  echo "Server status updated"
+  exit
+
+}
