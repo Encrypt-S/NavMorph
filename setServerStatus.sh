d2d8bdf7c17613132ff9ea4ef87e93b80552c212
Matt <matt@encrypt-s.com>
2017-09-19 16:49:43 +1200

    fixed script typo

--- setServerStatus.sh
+++ setServerStatus.sh
@@ -2,24 +2,22 @@
 
 # sysinfo_page - A script to update the server mode
 
-if [ $# lt 2 ]; then
-    echo "You're missing a server status argument."
-    echo "Usage: setServerStatus <status>"
-else if [ $# gt 2 ]; then
-    echo "You have used too many arguments."
-    echo "Usage: setServerStatus <status>"
-else
-  set_status()
-fi
-
-
 set_status () { 
   echo "Updating server status to $1"
   
-  mongo polymorph --eval "db.serverModes.update({}, {'server_mode': $1})"
-  mongo polymorph --eval "db.serverModes.update({}, {'server_mode': $1})"
+  mongo polymorph --eval "db.servermodes.update({}, {'server_mode': $1})"
+  mongo polymorph --eval "db.servermodes.update({}, {'server_mode': $1})"
   
   echo "Server status updated"
   exit
-
 }
+
+if [ $# -lt 1 ]; then 
+    echo "You're missing a server status argument."
+    echo "Usage: setServerStatus <status>"
+elif [ $# -gt 1 ]; then 
+    echo "You have used too many arguments."
+    echo "Usage: setServerStatus <status>"
+else 
+  set_status
+fi 
