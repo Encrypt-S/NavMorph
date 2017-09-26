7aeac5613917d9b09d25733157c973ea0e2d2b48
Matt <matt@encrypt-s.com>
2017-09-27 12:50:09 +1300

    Validator Complete.
    Tweaked server to use the new validator and not start up if validation fails. 
    Tweaked settings template to use caps and the correct types

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const https = require('https')
 const bodyParser = require('body-parser')
 const pem = require('pem')
 const mongoose = require('mongoose')
-const socketCtrl = require('./server/lib/socket/socketCtrl')
+const SocketCtrl = require('./server/lib/socket/socketCtrl')
+const SettingsValidator = require('./server/lib/settingsValidator.js')
 
 
 // Get our API routes
@@ -18,79 +19,97 @@ const config = require('./server-settings.json')
 
 const app = express()
 
-//validateConfig(config)
+/**
+  * Validate Server Settings Config(config)
+  */
 
-// Parsers for POST data
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+SettingsValidator.validateSettings(config)
+.then(() => {
+  console.log('--------------------------------------------')
+  console.log('Server Config Validated. Continuing start up')
+  console.log('--------------------------------------------')
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, config.app.static)))
-
-// Set our api routes
-app.use(config.app.apiUri, api)
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, config.app.catchAllUri))
+  startUpServer()
+})
+.catch((err) => {
+  console.log('--------------------------------------------')
+  console.log('ERR: Server Config invalid. Stopping start up')
+  console.log(err)
+  console.log('--------------------------------------------')
 })
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || config.serverPort
-app.set('port', port)
-
-/**
- * Create HTTPS server, set up sockets and listen on all network interfaces
- */
-
-var server
-var io
-
-pem.createCertificate(config.sslCert, (error, keys) => {
-  if (error) {
-    console.log('pem error: ' + error)
-  }
-  const sslOptions = {
-    key: keys.serviceKey,
-    cert: keys.certificate,
-    requestCert: false,
-    rejectUnauthorized: false,
-  }
+startUpServer = () => {
+  // Parsers for POST data
   app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({
-    extended: true,
-  }))
-  server = https.createServer(sslOptions, app)
-  io = require('socket.io')(server);
+  app.use(bodyParser.urlencoded({extended: false}))
 
-  socketCtrl.setupServerModeSocket(io)
-  .then(() => {
-    Logger.writeLog('n/a', 'Server Mode Socket Running', null, false)
+  // Point static path to dist
+  app.use(express.static(path.join(__dirname, config.app.static)))
+
+  // Set our api routes
+  app.use(config.app.apiUri, api)
+
+  // Catch all other routes and return the index file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, config.app.catchAllUri))
   })
-  .catch((err) => {
-    Logger.writeLog('001', 'Failed to start up Server Mode Socket', err, true)
+
+  /**
+   * Get port from environment and store in Express.
+   */
+  const port = process.env.PORT || config.serverPort
+  app.set('port', port)
+
+  /**
+   * Create HTTPS server, set up sockets and listen on all network interfaces
+   */
+
+  var server
+  var io
+
+  pem.createCertificate(config.sslCert, (error, keys) => {
+    if (error) {
+      console.log('pem error: ' + error)
+    }
+    const sslOptions = {
+      key: keys.serviceKey,
+      cert: keys.certificate,
+      requestCert: false,
+      rejectUnauthorized: false,
+    }
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+      extended: true,
+    }))
+    server = https.createServer(sslOptions, app)
+    io = require('socket.io')(server);
+
+    SocketCtrl.setupServerModeSocket(io)
+    .then(() => {
+      Logger.writeLog('n/a', 'Server Mode Socket Running', null, false)
+    })
+    .catch((err) => {
+      Logger.writeLog('001', 'Failed to start up Server Mode Socket', err, true)
+    })
+
+    server.listen(port, () => {
+      Logger.writeLog('n/a', `API running on https://localhost:${port}`, null, false)
+
+      /**
+      * Connect to mongoose
+      */
+      
+      mongoose.Promise = global.Promise
+      const mongoDB = config.mongoDBUrl
+      mongoose.connect(mongoDB)
+      const db = mongoose.connection
+      db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+      
+      Logger.writeLog('MongoDB Connect', `Conected to MongoDB on ${mongoDB}`, null, false)
+
+      Logger.writeLog('n/a', 'Sending start up notification email.', null, false)
+      Logger.writeLog('Server Start Up', 'Start Up Complete @' + new Date().toISOString() +
+        ', Polymorph Version: ' + config.version, null, true)
+    })
   })
-
-  server.listen(port, () => {
-    Logger.writeLog('n/a', `API running on https://localhost:${port}`, null, false)
-
-    /**
-    * Connect to mongoose
-    */
-    
-    mongoose.Promise = global.Promise
-    const mongoDB = config.mongoDBUrl
-    mongoose.connect(mongoDB)
-    const db = mongoose.connection
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-    
-    Logger.writeLog('MongoDB Connect', `Conected to MongoDB on ${mongoDB}`, null, false)
-
-    Logger.writeLog('n/a', 'Sending start up notification email.', null, false)
-    Logger.writeLog('Server Start Up', 'Start Up Complete @' + new Date().toISOString() +
-      ', Polymorph Version: ' + config.version, null, true)
-  })
-})
-
+}
