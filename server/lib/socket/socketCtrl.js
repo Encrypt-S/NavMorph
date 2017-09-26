4322a8aa977ca2cd19bcbaaa0183d5b89f08b1a9
Matt <matt@encrypt-s.com>
2017-09-26 15:38:20 +1300

    added a new db model for server messages. updated the socket method names to be more agnostic. Added a method to check db messages

--- server/lib/socket/socketCtrl.js
+++ server/lib/socket/socketCtrl.js
@@ -4,7 +4,7 @@ serverModeCtrl = require('../db/serverMode.ctrl')
 
 socketCtrl = {}
     
-  socketCtrl.setupServerModeSocket = (socket) => {
+  socketCtrl.setupServerSocket = (socket) => {
     return new Promise((fufill, reject) => {    
       try {
         socket.on('connection', function(socket){
@@ -16,7 +16,7 @@ socketCtrl = {}
             socket.emit('message', {type:'new-message', text: message})
           })
         })
-        socketCtrl.startServerModeWatch(socket)
+        socketCtrl.startDbWatch(socket)
         fufill()
       } catch (e) {
         reject(e)
@@ -24,11 +24,21 @@ socketCtrl = {}
     })
   }
 
-  socketCtrl.startServerModeWatch = (socket) => {
+  socketCtrl.startDbWatch = (socket) => {
     setInterval(() => {
       serverModeCtrl.checkMode()
       .then((mode) => {
         socket.emit('server-mode', mode[0].server_mode)
+      })
+      .then(() => {
+        return serverModeCtrl.checkMessage()
+      })
+      .then((serverMessageData) => {
+        socket.emit('server-message', {
+          serverMessage: serverMessageData[0].server_message,
+          serverMessageType: serverMessageData[0].message_type,
+          showMessage: serverMessageData[0].show_message,
+        })
       })
     }, 1000)
   }
