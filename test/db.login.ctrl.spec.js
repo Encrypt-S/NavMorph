f66fc839da9d11381b0700906ad3437fb01d6291
Matt <matt@encrypt-s.com>
2017-10-03 10:37:14 +1300

    reworked tests, removed uneeded tests, added more tests

--- test/db.login.ctrl.spec.js
+++ test/db.login.ctrl.spec.js
@@ -14,6 +14,7 @@ describe('[Login.Ctrl]', () => {
       mockLogger = { writeLog: sinon.spy() }
       LoginCtrl.__set__('Logger', mockLogger)
     })
+
     it('should fail on params', (done) => {
       LoginCtrl.insertAttempt({ junkParam: '1234' })
         .catch((error) => {
@@ -23,6 +24,7 @@ describe('[Login.Ctrl]', () => {
           done()
         })
     })
+
     it('should get correct params', (done) => {
       const ipAddress = '192.168.10.1'
       const polymorphId = '123'
@@ -32,7 +34,7 @@ describe('[Login.Ctrl]', () => {
         mockFailedLoginsModel.params = paramsToSave
       }
 
-      LoginCtrl.insertSave = (fulfill, reject) => {
+      LoginCtrl.executeSave = (fulfill, reject) => {
         expect(mockFailedLoginsModel.params.ip_address).toBe(ipAddress)
         expect(mockFailedLoginsModel.params.polymorph_id).toBe(polymorphId)
         expect(mockFailedLoginsModel.params.params).toBe(JSON.stringify(params))
@@ -45,12 +47,54 @@ describe('[Login.Ctrl]', () => {
       LoginCtrl.insertAttempt({ ipAddress, polymorphId, params })
     })
   })
-  describe('(insertSave)', () => {
+  
+  describe('(blackListIp)', () => {
     beforeEach(() => { // reset the rewired functions
       LoginCtrl = rewire('../server/lib/db/login.ctrl')
       mockLogger = { writeLog: sinon.spy() }
       LoginCtrl.__set__('Logger', mockLogger)
     })
+
+    it('should fail on params', (done) => {
+      LoginCtrl.blackListIp({ junkParam: '1234' })
+        .catch((error) => {
+          expect(error).toBe('LGN_004')
+          sinon.assert.calledOnce(mockLogger.writeLog)
+          sinon.assert.calledWith(mockLogger.writeLog, 'LGN_004')
+          done()
+        })
+    })
+
+    it('should pass on params', (done) => {
+      const blacklistSpy = sinon.spy(LoginCtrl, 'executeSave')
+      const ipAddress = '1.1.1.1'
+
+      mockLogger = { writeLog: sinon.spy() }
+      LoginCtrl.__set__('Logger', mockLogger)
+
+      function mockBlacklistModel(paramsToSave) {
+        mockBlacklistModel.params = paramsToSave
+      }
+
+      LoginCtrl.executeSave = (fulfill, reject) => {
+        expect(LoginCtrl.runtime.transaction.ip_address).toBe(ipAddress)
+        expect(LoginCtrl.runtime.transaction.timestamp instanceof Date).toBe(true)
+        sinon.assert.notCalled(mockLogger.writeLog)
+        done()
+      }
+
+      LoginCtrl.__set__('BlackListModel', mockBlacklistModel)
+      LoginCtrl.blackListIp({ ipAddress })
+    })
+  })
+
+  describe('(executeSave)', () => {
+    beforeEach(() => { // reset the rewired functions
+      LoginCtrl = rewire('../server/lib/db/login.ctrl')
+      mockLogger = { writeLog: sinon.spy() }
+      LoginCtrl.__set__('Logger', mockLogger)
+    })
+
     it('should handle save success', (done) => {
 
       LoginCtrl.runtime.transaction = {
@@ -63,8 +107,9 @@ describe('[Login.Ctrl]', () => {
         done()
       }
 
-      LoginCtrl.insertSave(fulfill, null)
+      LoginCtrl.executeSave(fulfill, null)
     })
+
     it('should handle save failure', (done) => {
       LoginCtrl.runtime.transaction = {
         save: () => { return Promise.reject(new Error('SAVE_FAILED')) },
@@ -76,9 +121,9 @@ describe('[Login.Ctrl]', () => {
         sinon.assert.calledWith(mockLogger.writeLog, 'LGN_002')
         done()
       }
-
-      LoginCtrl.insertSave(null, reject)
+      LoginCtrl.executeSave(null, reject)
     })
+
     it('should handle exceptions', (done) => {
       function Exception(message){
         this.error = message
@@ -95,102 +140,96 @@ describe('[Login.Ctrl]', () => {
         done()
       }
 
-      LoginCtrl.insertSave(null, reject)
+      LoginCtrl.executeSave(null, reject)
     })
   })
 
-  describe('(blackListIp)', () => {
+  describe('(checkIpBlocked)', () => {
     beforeEach(() => { // reset the rewired functions
       LoginCtrl = rewire('../server/lib/db/login.ctrl')
       mockLogger = { writeLog: sinon.spy() }
       LoginCtrl.__set__('Logger', mockLogger)
     })
+
     it('should fail on params', (done) => {
-      LoginCtrl.blackListIp({ junkParam: '1234' })
+      LoginCtrl.checkIpBlocked({ junkParam: '1234' })
         .catch((error) => {
-          expect(error).toBe('LGN_004')
+          expect(error).toBe('LGN_005')
           sinon.assert.calledOnce(mockLogger.writeLog)
-          sinon.assert.calledWith(mockLogger.writeLog, 'LGN_004')
+          sinon.assert.calledWith(mockLogger.writeLog, 'LGN_005')
           done()
         })
     })
 
-    it('should pass on params', (done) => {
-      const blacklistSpy = sinon.spy(LoginCtrl, 'insertIntoBlacklist')
-      const ipAddress = '1.1.1.1'
+    // it('should pass on params', (done) => {
+    //   const executeIpSpy = sinon.spy(LoginCtrl, 'executeIpBlockedQuery')
+    //   const ipAddress = '1.1.1.1'
 
-      mockLogger = { writeLog: sinon.spy() }
-      LoginCtrl.__set__('Logger', mockLogger)
+    //   mockLogger = { writeLog: sinon.spy() }
+    //   LoginCtrl.__set__('Logger', mockLogger)
 
-      function mockBlacklistModel(paramsToSave) {
-        mockBlacklistModel.params = paramsToSave
-      }
+    //   const mockBlacklistModel = {
+    //     find: {
+    //       and: (paramsToSave) => {
+    //         console.log(paramsToSave)
+    //       },
+    //     },
+        
+    //   }
 
-      LoginCtrl.insertIntoBlacklist = (fulfill, reject) => {
-        expect(LoginCtrl.runtime.transaction.ip_address).toBe(ipAddress)
-        expect(LoginCtrl.runtime.transaction.timestamp instanceof Date).toBe(true)
-        sinon.assert.notCalled(mockLogger.writeLog)
-        done()
-      }
+    //   LoginCtrl.executeIpBlockedQuery = (options) => {
+    //     // console.log('options.query', options.query)
+    //     expect(options.ip_address).toBe(ipAddress)
+    //     expect(options.timestamp instanceof Date).toBe(true)
+    //     sinon.assert.notCalled(mockLogger.writeLog)
+    //     done()
+    //   }
 
-      LoginCtrl.__set__('BlackListModel', mockBlacklistModel)
-      LoginCtrl.blackListIp({ ipAddress })
-    })
+    //   LoginCtrl.__set__('BlackListModel', mockBlacklistModel)
+
+    //   LoginCtrl.checkIpBlocked({ ipAddress })
+    // })
   })
 
-  describe('(insertSave)', () => {
+  describe('(executeIpBlockedQuery)', () => {
     beforeEach(() => { // reset the rewired functions
       LoginCtrl = rewire('../server/lib/db/login.ctrl')
-      mockLogger = { writeLog: sinon.spy() }
-      LoginCtrl.__set__('Logger', mockLogger)
     })
-    it('should handle save success', (done) => {
 
-      LoginCtrl.runtime.transaction = {
-        save: () => { return Promise.resolve('SUCCESS') },
+    it('should handle query success', (done) => {
+      let query = {}
+      query.exec = () => {
+        return Promise.resolve('SUCCESS')
       }
 
-      function fulfill(result) {
-        expect(result).toBe('SUCCESS')
-        sinon.assert.notCalled(mockLogger.writeLog)
+      LoginCtrl.checkResults = (options) => {
+        expect(options.result).toBe('SUCCESS')
+        expect(options.result).toBe('SUCCESS')
+        expect(options.limit).toBe(0)
         done()
       }
 
-      LoginCtrl.insertIntoBlacklist(fulfill, null)
-    })
-    
-    it('should handle save failure', (done) => {
-      LoginCtrl.runtime.transaction = {
-        save: () => { return Promise.reject(new Error('SAVE_FAILED')) },
-      }
+      LoginCtrl.executeIpBlockedQuery({ query })
+    }) 
 
-      function reject(result) {
-        expect(result.message).toBe('SAVE_FAILED')
-        sinon.assert.calledOnce(mockLogger.writeLog)
-        sinon.assert.calledWith(mockLogger.writeLog, 'LGN_005')
-        done()
-      }
+    // it('should handle query failure', (done) => {
+    //   let query = {}
+    //   query.exec = () => {
+    //     return Promise.reject('QUERY_FAILED')
+    //   }
 
-      LoginCtrl.insertIntoBlacklist(null, reject)
-    })
+    //   LoginCtrl.runtime.transaction = {
+    //     save: () => { return Promise.reject(new Error('QUERY_FAILED')) },
+    //   }
 
-    it('should handle exceptions', (done) => {
-      function Exception(message){
-        this.error = message
-      }
-
-      LoginCtrl.runtime.transaction = {
-        save: () => { throw new Exception('SAVE_EXCEPTION') },
-      }
-
-      function reject(result) {
-        expect(result.error).toBe('SAVE_EXCEPTION')
-        sinon.assert.calledOnce(mockLogger.writeLog)
-        sinon.assert.calledWith(mockLogger.writeLog, 'LGN_006')
-        done()
-      }
-
-      LoginCtrl.insertIntoBlacklist(null, reject)
-    })
+    //   function reject(result) {
+    //     expect(result.message).toBe('QUERY_FAILED')
+    //     sinon.assert.calledOnce(mockLogger.writeLog)
+    //     sinon.assert.calledWith(mockLogger.writeLog, 'LGN_002')
+    //     done()
+    //   }
+    //   LoginCtrl.executeIpBlockedQuery({ query, null, reject })
+    // })
   })
+
 })
